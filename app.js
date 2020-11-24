ac044db0a4dad57a1cfe805f7e8cd873102a8a7c
pubudu <pubudukaviranga20@gmail.com>
2020-11-24 18:25:37 +0530

    Refactor code

--- app.js
+++ app.js
@@ -1,53 +1,44 @@
-const express = require ("express");
-const bodyParser = require ("body-parser");
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const app = express();
 
-var items = ["Buy food","Cook food", "Eat food"];
-var workItems = [];
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
-
-  var today = new  Date();
-  var options = {
-    weekday: "long",
-    day : "numeric",
-    month: "long"
-  };
-  var day = today.toLocaleDateString("en-US",options);
-
-  res.render("list", {listTitle:day, newListItems: items});
+app.get("/", function(req, res) {
+  const day = date.getDate();
+  res.render("list", {listTitle: day,newListItems: items});
 });
 
-app.post("/", function(req,res){
-  let item = req.body.newItem;
-  console.log(req.body);
-  if(req.body.list === "work"){
+app.post("/", function(req, res) {
+  const item = req.body.newItem;
+  if (req.body.list === "work") {
     workItems.push(item);
     res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
   }
-  else{
-  items.push(item);
-  res.redirect("/");
-  }
-// }
-
 });
 
-app.get("/work", function(req,res){
-  res.render("list",{listTitle: "work List", newListItems:workItems});
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "work List",
+    newListItems: workItems
+  });
 });
 
-app.get("/about", function(req,res){
+app.get("/about", function(req, res) {
   res.render("about");
 })
 
 
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("server started on port 3000")
 });
